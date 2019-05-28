09705e72562dfde3c8b43041dfc1046ee8efce89
Wayne <waynelee1992@gmail.com>
2019-05-28 15:18:57 +0800

    Added more data for scanned

--- asset.py
+++ asset.py
@@ -1,40 +1,75 @@
 import wmi
+import sys
 import json
+import re
+# import csv
 
+from pprint import pprint as pp
 from win32com.client import GetObject
-
-objWMI = GetObject('winmgmts:\\\\.\\root\\SecurityCenter2').InstancesOf('AntiVirusProduct')
 
 c = wmi.WMI()
 
+objWMI = GetObject('winmgmts:\\\\.\\root\\SecurityCenter2').InstancesOf('AntiVirusProduct')
+
+wql = "SELECT IPAddress FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = 'True'"
+
+hostname = c.Win32_ComputerSystem()[0].Caption
+os = c.Win32_OperatingSystem()[0].Caption
 cpu = c.Win32_Processor()[0].Name
-
+ram = int(int(c.Win32_ComputerSystem()[0].TotalPhysicalMemory) / 1000000)
+ip = [ip.IPAddress for ip in c.query(wql)]
+installedapps = c.Win32_Product()
 displayname = ''
-instanceguid = ''
-productState = ''
-timestamp = ''
+avdetected = ''
+locationfound = False
 
+# Removing non-string name fields to allow regex iteration
+for index, app in enumerate(installedapps):
+    if app.Name == None:
+        installedapps.pop(index)
+
+# Detecting Anti Virus presence and name
 for obj in objWMI:
-	if obj.displayName != None:
-		displayname = str(obj.displayName)
-	if obj.instanceGuid != None:
-		instanceguid = str(obj.instanceGuid)
-	if obj.productState != None:
-		productState = str(obj.productState)
-	if obj.timestamp != None:
-		timestamp = str(obj.timestamp)
-	print("")
-	print("########")
-	print("")
+    if obj.displayName != None:
+        displayname = str(obj.displayName)
+        avdetected = True
 
-data = [{"CPU": cpu, "AV": displayname, "State": productState,}]
+# Search for installed location of Anti Virus
+# if displayname != 'Windows Defender' and avdetected == True:
+for app in installedapps:
+    searchav = displayname + '.*'
+    p = re.compile(searchav)
+    m = p.match(app.Name)
+    if m:
 
-with open('test.json', 'w') as f:
-	json.dump(data, f, indent=4)
+        location = app.InstallLocation
+        locationfound = True
 
-print("CPU: " + cpu)
-print("Anti Virus: " + displayname)
-print("Product State: " + productState)
-print("Time Stamp: " + timestamp)
+# Assigning collected data to variable
 
-input("Press Enter to continue...")
+data = [{
+    'HostName': hostname,
+    'OperatingSystem': os,
+    'CPU': cpu,
+    'RAM': ram,
+    'IPAddress': ip,
+    'AntiVirus': displayname,
+}]
+
+if locationfound == True:
+    data[0].update({'Location': location})
+
+
+with open(hostname + '.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
+# CSV OUTPUT
+# with open(hostname + '.csv', 'w') as w:
+#     fields = ['HostName', 'OperatingSystem', 'RAM', 'IPAddress', 'AntiVirus', 'Version', 'InstallationDate', 'Location']
+#     writer = csv.DictWriter(w, fieldnames=fields)
+#     writer.writeheader()
+#     writer.writerows(data)
+
+# w.close()
+
+pp(data)
